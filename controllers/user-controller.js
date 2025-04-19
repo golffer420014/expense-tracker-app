55c13ee155692c4994034da2a41c2eabc836ec28
Suriya <142491843+golffer420014@users.noreply.github.com>
2025-04-20 01:15:26 +0700

    update api

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -31,6 +31,60 @@ const register = async (req, res) => {
     }
 }
 
+const getAll = async (req, res) => {
+    try {
+        const users = await userSchema.find();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error('Error getting users:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+const getByID = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await userSchema.findOne(`WHERE id = '${id}'`);
+
+        if (!user) { return res.status(404).json({ message: 'User not found.' }); }
+
+        res.status(200).json(user);
+    } catch (error) {
+        console.error('Error getting user:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const { username, name, password } = req.body;
+
+        const user = await userSchema.findOne(`WHERE id = '${id}'`);
+
+        if (!user) { return res.status(404).json({ message: 'User not found.' }); }
+
+        const updatedUser = await userSchema.update(
+            {
+                username: username || user.username,
+                name: name || user.name,
+                password_hash: password ? await argon2.hash(password) : user.password_hash
+            },
+            `id = '${id}'`
+        );
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.error('Error updating user:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
+
 module.exports = {
-    register
+    register,
+    getAll,
+    getByID,
+    updateUser
 }
--- route/user.js
+++ route/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controllers/user-controller');
+const { register, getAll, getByID, updateUser } = require('../controllers/user-controller');
 
 
 // POST /users - Create a new user
 router.post('/register', register);
+router.get('/get-all', getAll);
+router.get('/get-by-id/:id', getByID);
+router.post('/update-user/:id', updateUser);
 
 module.exports = router;
