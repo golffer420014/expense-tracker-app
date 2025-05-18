561cfa9983bb92229c2ba6fd2feb1e70825cb42c
Suriya <142491843+golffer420014@users.noreply.github.com>
2025-05-19 00:43:02 +0700

    Update pool.js

--- api/lib/pool.js
+++ api/lib/pool.js
@@ -1,17 +1,17 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
+require("dotenv").config();
 
 // ตั้งค่าการเชื่อมต่อกับ Neon PostgreSQL
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   // http
-  // ssl: process.env.NODE_ENV === 'production' 
+  // ssl: process.env.NODE_ENV === 'production'
   //   ? { rejectUnauthorized: false, sslmode: 'require' }
   //   : false
-// https
-    ssl: {
-      rejectUnauthorized: false
-    }
+  // https
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
